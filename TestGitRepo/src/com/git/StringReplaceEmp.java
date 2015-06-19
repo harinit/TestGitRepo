60c65eff91cfb46a0f963bcae4809ee95b1d66d4
Harini T <Harini.Thangirala@cognizant.com>
2015-06-19 09:50:44 +0530

    The TestGitRepo is to test the project in another machine.

---
+++ TestGitRepo/src/com/git/StringReplaceEmp.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.git;
+
+/**
+ * @author 282026
+ *
+ */
+public class StringReplaceEmp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String str = "Hello Wellpoint";
+		System.out.println(str.replace("Wellpoint", "Anthem"));
+	}
+
+}
